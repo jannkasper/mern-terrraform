0cfa8399649cd5ea60e87284e24d7455689e8fa6
jkasper <jan.kasper@pt.software>
2023-10-22 17:40:40 +0200

    test

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,7 +7,10 @@ const api = axios.create({
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
-export const getAllMovies = () => api.get(`/movies`)
+export const getAllMovies = function () {
+    console.log(`serverUrl: ${serverUrl}`)
+    return api.get(`/movies`)
+}
 export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload)
 export const deleteMovieById = id => api.delete(`/movie/${id}`)
 export const getMovieById = id => api.get(`/movie/${id}`)
